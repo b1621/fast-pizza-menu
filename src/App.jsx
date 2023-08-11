ed8e3178e6856bcbc6e292adffdbdfc22f6f3c0f
b0 <bewuket1621@gmail.com>
2023-08-11 09:23:49 -0400

    a new way of implementing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/Cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order",
+    element: <Order />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h2 className="text-center text-4xl my-10">Hello world</h2>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
