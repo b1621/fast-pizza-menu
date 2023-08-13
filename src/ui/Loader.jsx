b105db957b7161a072cda4c9e7615112e74cfba2
b1621 <bewuket1621@gmail.com>
2023-08-13 22:01:59 +0300

    updating the loading component

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,9 @@
 const Loader = () => {
   return (
-    <div className="absolute  top-52 w-full">
-      <p className="mx-auto my-10 w-fit text-3xl">Loading ....</p>
+    // inset-0 = top-0 bottom-0 left-0 right-0
+    <div className="absolute inset-0 flex w-full items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <p className="mr-5 w-fit text-5xl font-semibold">Loading </p>
+      <div className="loader h-7"></div>
     </div>
   );
 };
