4291b60cd3ffba9e3f1b1d43e48ba05b8b9107d4
b0 <bewuket1621@gmail.com>
2023-08-14 05:43:16 -0400

    reading and updating the user state

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
@@ -46,7 +45,7 @@ function CreateOrder() {
 
   return (
     <div className=" px-4 py-6">
-      <h2 className="mb-8 text-2xl font-semibold">Ready to order? Let's go!</h2>
+      <h2 className="mb-8 text-2xl font-semibold">Ready to order? Lets go!</h2>
 
       {/* <Form method="POST" action="/order/new">  */}
       <Form method="POST">
