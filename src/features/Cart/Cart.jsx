c4a238dd7b5322034f75cb37866c6b52b20f06d5
b0 <bewuket1621@gmail.com>
2023-08-14 09:19:05 -0400

    building the cart page

--- src/features/Cart/Cart.jsx
+++ src/features/Cart/Cart.jsx
@@ -2,35 +2,23 @@ import { Link } from "react-router-dom";
 import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart } from "./cartSlice";
+import EmptyCart from "./EmptyCart";
 
 function Cart() {
-  const cart = fakeCart;
+  // const cart = fakeCart;
+  const dispatch = useDispatch();
+
+  const cart = useSelector(getCart);
   const username = useSelector((state) => state.user.username);
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  // if (!cart || cart === []) return <EmptyCart />;
+  if (!cart.length) return <EmptyCart />;
   return (
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
@@ -47,7 +35,9 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear Cart</Button>
+        <Button type="secondary" onClick={handleClearCart}>
+          Clear Cart
+        </Button>
         {/* <button>Clear cart</button> */}
       </div>
     </div>
